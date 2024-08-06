a973c026521520037f79ef722b4dd410f1bd54ec
sara-johnson <sara-johnson@users.noreply.github.com>
2024-08-05 22:03:13 -0700

    HoistingVariables

---
+++ HoistingVariables.ts
@@ -0,0 +1,11 @@
+//hoisting. It is a quirk of JavaScript that brings all declarations made with var to the top of the function (or into the global scope if declared outside a function).
+x = "not declared before assignment"; 
+var x = "declared after assignment  and all fine"; 
+console.log(x)
+
+
+
+
+
+
+
